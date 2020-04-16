37c94533c1532e02259dbfef4c5872dfd58a15c0
Diana Kriese <diana.kriese@caci.com>
2020-04-16 14:30:31 -0600

    adding fake-db files for hard-coded back end source

---
+++ src/app/fake-db/fa-dictionary.ts
@@ -0,0 +1,419 @@
+export class FaDictionary {
+    public static data = [
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Atlanta Falcons",
+            "Entity_Type": "Organization",
+            "Description": "",
+            "Alternate_Names": [
+                "Falcons"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Arizona Cardinals",
+            "Entity_Type": "Organization",
+            "Description": "",
+            "Alternate_Names": [
+                "Cardinals"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Baltimore Ravens",
+            "Entity_Type": "Organization",
+            "Description": "",
+            "Alternate_Names": [
+                "Ravens"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Buffalo Bills",
+            "Entity_Type": "Organization",
+            "Description": "",
+            "Alternate_Names": [
+                "Bills"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Carolina Panthers",
+            "Entity_Type": "Organization",
+            "Description": "",
+            "Alternate_Names": [
+                "Panthers"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Chicago Bears",
+            "Entity_Type": "Organization",
+            "Description": "",
+            "Alternate_Names": [
+                "Bears"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Cincinnati Bengals",
+            "Entity_Type": "Organization",
+            "Description": "",
+            "Alternate_Names": [
+                "Bengals"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Cleveland Browns",
+            "Entity_Type": "Organization",
+            "Description": "",
+            "Alternate_Names": [
+                "Browns"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Dallas Cowboys",
+            "Entity_Type": "Organization",
+            "Description": "",
+            "Alternate_Names": [
+                "Cowboys"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Denver Broncos",
+            "Entity_Type": "Organization",
+            "Description": "",
+            "Alternate_Names": [
+                "Broncos"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Detroit Lions",
+            "Entity_Type": "Organization",
+            "Description": "",
+            "Alternate_Names": [
+                "Lions"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Green Bay Packers",
+            "Entity_Type": "Organization",
+            "Description": "",
+            "Alternate_Names": [
+                "Packers"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Houston Texans",
+            "Entity_Type": "Organization",
+            "Description": "",
+            "Alternate_Names": [
+                "Texans"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Indianapolis Colts",
+            "Entity_Type": "Organization",
+            "Description": "",
+            "Alternate_Names": [
+                "Colts"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Jacksonville Jaguars",
+            "Entity_Type": "Organization",
+            "Description": "",
+            "Alternate_Names": [
+                "Jaguars"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Kansas City Chiefs",
+            "Entity_Type": "Organization",
+            "Description": "",
+            "Alternate_Names": [
+                "City Chiefs"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Los Angeles Chargers",
+            "Entity_Type": "Organization",
+            "Description": "",
+            "Alternate_Names": [
+                "Chargers"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Los Angeles Rams",
+            "Entity_Type": "Organization",
+            "Description": "",
+            "Alternate_Names": [
+                "Rams"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Miami Dolphins",
+            "Entity_Type": "Organization",
+            "Description": "",
+            "Alternate_Names": [
+                "Dolphins"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Minnesota Vikings",
+            "Entity_Type": "Organization",
+            "Description": "",
+            "Alternate_Names": [
+                "Vikings"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "New England Patriots",
+            "Entity_Type": "Organization",
+            "Description": "",
+            "Alternate_Names": [
+                "Patriots"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "New Orleans Saints",
+            "Entity_Type": "Organization",
+            "Description": "",
+            "Alternate_Names": [
+                "Saints"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "NY Giants",
+            "Entity_Type": "Organization",
+            "Description": "",
+            "Alternate_Names": [
+                "Giants"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "NY Jets",
+            "Entity_Type": "Organization",
+            "Description": "",
+            "Alternate_Names": [
+                "Jets"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Oakland Raiders",
+            "Entity_Type": "Organization",
+            "Description": "",
+            "Alternate_Names": [
+                "Raiders"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Philadelphia Eagles",
+            "Entity_Type": "Organization",
+            "Description": "",
+            "Alternate_Names": [
+                "Eagles"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Pittsburgh Steelers",
+            "Entity_Type": "Organization",
+            "Description": "",
+            "Alternate_Names": [
+                "Steelers"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "San Francisco 49ers radio",
+            "Entity_Type": "Organization",
+            "Description": "",
+            "Alternate_Names": [
+                "49ers"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Seattle Seahawks",
+            "Entity_Type": "Organization",
+            "Description": "",
+            "Alternate_Names": [
+                "Seahawks"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Tampa Bay Buccaneers",
+            "Entity_Type": "Organization",
+            "Description": "",
+            "Alternate_Names": [
+                "Buccaneers"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Tennessee Titans",
+            "Entity_Type": "Organization",
+            "Description": "",
+            "Alternate_Names": [
+                "Titans"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Washington Redskins",
+            "Entity_Type": "Organization",
+            "Description": "",
+            "Alternate_Names": [
+                "Redskins",
+                "Skins"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Tom Brady",
+            "Entity_Type": "People",
+            "Description": "",
+            "Alternate_Names": [
+                "Brady"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Aaron Donale",
+            "Entity_Type": "People",
+            "Description": "",
+            "Alternate_Names": [
+                "Donald"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Drew Brees",
+            "Entity_Type": "People",
+            "Description": "",
+            "Alternate_Names": [
+                "Brees"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Khalil Mack",
+            "Entity_Type": "People",
+            "Description": "",
+            "Alternate_Names": [
+                "Mack"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Pat Mahomes",
+            "Entity_Type": "People",
+            "Description": "",
+            "Alternate_Names": [
+                "Mahomes"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Tom Gurley",
+            "Entity_Type": "People",
+            "Description": "",
+            "Alternate_Names": [
+                "Gurley"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Antonio Brown",
+            "Entity_Type": "People",
+            "Description": "",
+            "Alternate_Names": [
+                "Browns"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Aaron Rodgers",
+            "Entity_Type": "People",
+            "Description": "",
+            "Alternate_Names": [
+                "Rodgers"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Julio Jones",
+            "Entity_Type": "People",
+            "Description": "",
+            "Alternate_Names": [
+                "Jones"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Bill Belichick",
+            "Entity_Type": "People",
+            "Description": "",
+            "Alternate_Names": [
+                "Belichick"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Andy Reid",
+            "Entity_Type": "People",
+            "Description": "",
+            "Alternate_Names": [
+                "Reid"
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Mercedes-Benz Stadium",
+            "Entity_Type": "Venue",
+            "Description": "",
+            "Alternate_Names": [
+                ""
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Hard Rock Stadium",
+            "Entity_Type": "Venue",
+            "Description": "",
+            "Alternate_Names": [
+                ""
+            ]
+        },
+        {
+            "COI": "Deep OBP - NFL",
+            "Name": "Super Bowl LIV",
+            "Entity_Type": "Event",
+            "Description": "",
+            "Alternate_Names": [
+                ""
+            ]
+        }
+    ]
+}
